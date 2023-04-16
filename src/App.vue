823a6349c40acf77407d9c667e3705e6c27e63e6
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2023-04-16 15:37:56 +0200

    added some initial data to game store and test implementation in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
+import { useGameStore } from '@/stores/game'
 import GameCanvas from '@/components/GameCanvas.vue'
+
+const game = useGameStore()
 </script>
 
 <template>
--- src/stores/game.js
+++ src/stores/game.js
@@ -1,3 +1,21 @@
+import { reactive } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useGameStore = defineStore('game', () => {})
+function createInitialState() {
+  return {
+    stage: 'before', // 'before' | 'playing' | 'after'
+    score: 0,
+    level: 0,
+    lines: 0,
+    currentTetromino: null,
+    nextTetromino: null
+  }
+}
+
+export const useGameStore = defineStore('game', () => {
+  const status = reactive(createInitialState())
+
+  return {
+    status
+  }
+})
