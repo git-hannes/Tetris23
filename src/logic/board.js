928f1113dbcd4888a1c65e15dfac9143487559b6
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2023-04-16 16:01:04 +0200

    created board class

---
+++ src/logic/board.js
@@ -0,0 +1,57 @@
+export class Board {
+  constructor(rows, cols) {
+    this.rows = rows
+    this.cols = cols
+    this.boardMatrix = this.createEmptyBoard()
+  }
+
+  createEmptyBoard() {
+    return Array.from({ length: this.rows }, () => Array(this.cols).fill(0))
+  }
+
+  isValidMove(tetromino, posX, posY) {
+    for (let y = 0; y < tetromino.shape.length; y++) {
+      for (let x = 0; x < tetromino.shape[y].length; x++) {
+        if (
+          tetromino.shape[y][x] &&
+          (posY + y < 0 ||
+            posX + x < 0 ||
+            posX + x >= this.cols ||
+            posY + y >= this.rows ||
+            this.boardMatrix[posY + y][posX + x] !== 0)
+        ) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+
+  lockPiece(tetromino, posX, posY) {
+    for (let y = 0; y < tetromino.shape.length; y++) {
+      for (let x = 0; x < tetromino.shape[y].length; x++) {
+        if (tetromino.shape[y][x]) {
+          this.boardMatrix[posY + y][posX + x] = tetromino.color
+        }
+      }
+    }
+  }
+
+  clearLines() {
+    let linesCleared = 0
+
+    outer: for (let y = this.rows - 1; y >= 0; ) {
+      for (let x = 0; x < this.cols; x++) {
+        if (this.boardMatrix[y][x] === 0) {
+          continue outer
+        }
+      }
+
+      linesCleared++
+      this.boardMatrix.splice(y, 1)
+      this.boardMatrix.unshift(Array(this.cols).fill(0))
+    }
+
+    return linesCleared
+  }
+}
