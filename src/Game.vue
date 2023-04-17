2854533d9993fcf303753aa7dd2888f4d05bf261
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2023-04-17 20:17:50 +0200

    formatted text

--- src/Game.vue
+++ src/Game.vue
@@ -1,60 +1,73 @@
 <script setup>
-import { onMounted, ref, onUnmounted } from "vue";
-import { handleKeyDown } from "@/controls.js";
-import { CANVAS_WIDTH, CANVAS_HEIGHT } from "@/constants/misc.js";
-import { drawBoard, drawTetromino, drawGhost } from "@/game/draw.js";
+import { onMounted, ref, onUnmounted } from 'vue'
+import { handleKeyDown } from '@/controls.js'
+import { CANVAS_WIDTH, CANVAS_HEIGHT } from '@/constants/misc.js'
+import { drawBoard, drawTetromino, drawGhost } from '@/game/draw.js'
 
-import ScreenOverlay from "@/components/ScreenOverlay.vue";
-import { useGameStore, useSettingsStore } from "@/stores";
+import ScreenOverlay from '@/components/ScreenOverlay.vue'
+import { useGameStore, useSettingsStore } from '@/stores'
 
-const GAME = useGameStore();
-const SETTINGS = useSettingsStore();
-let canvas = ref(null);
+const GAME = useGameStore()
+const SETTINGS = useSettingsStore()
+let canvas = ref(null)
 
 function gameLoop(ctx, timestamp) {
   // I probably shouldn't be doing this inside the game loop, but declaring it
   // outside the loop causes a bug
-  const TETROMINO = GAME.tetromino;
+  const TETROMINO = GAME.tetromino
 
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  drawBoard(ctx, GAME);
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  drawBoard(ctx, GAME)
 
-  if (GAME.state.stage === "playing") {
-    drawTetromino(ctx, TETROMINO.current, TETROMINO.current.shape);
-    drawGhost(ctx, GAME, TETROMINO.current, SETTINGS);
+  if (GAME.state.stage === 'playing') {
+    drawTetromino(ctx, TETROMINO.current, TETROMINO.current.shape)
+    drawGhost(ctx, GAME, TETROMINO.current, SETTINGS)
 
-    const fallingSpeed = 1000 - GAME.state.level * 50;
+    const fallingSpeed = 1000 - GAME.state.level * 50
     if (timestamp - TETROMINO.lastDropTime > fallingSpeed) {
-      TETROMINO.current.move("DOWN");
-      TETROMINO.lastDropTime = timestamp;
+      TETROMINO.current.move('DOWN')
+      TETROMINO.lastDropTime = timestamp
     }
   }
 
-  requestAnimationFrame(gameLoop.bind(null, ctx));
+  requestAnimationFrame(gameLoop.bind(null, ctx))
 }
 
 onMounted(() => {
-  const ctx = canvas.value.getContext("2d");
-  gameLoop(ctx, 0);
-  window.addEventListener("keydown", handleKeyDown);
-});
+  const ctx = canvas.value.getContext('2d')
+  gameLoop(ctx, 0)
+  window.addEventListener('keydown', handleKeyDown)
+})
 
 onUnmounted(() => {
-  window.removeEventListener("keydown", handleKeyDown);
-});
+  window.removeEventListener('keydown', handleKeyDown)
+})
 </script>
 
 <template>
-  <div class="game-container relative mx-auto" :style="{ width: `${CANVAS_WIDTH}px`, height: `${CANVAS_HEIGHT}px` }">
+  <div
+    class="game-container relative mx-auto"
+    :style="{ width: `${CANVAS_WIDTH}px`, height: `${CANVAS_HEIGHT}px` }"
+  >
     <ScreenOverlay v-if="GAME.state.stage === 'before'">
-      <p>Press <span class="bg-indigo-500 px-2">SPACE</span> to start</p>
+      <p>
+        Press<br />
+        <span class="bg-indigo-500 px-2">SPACE</span><br />
+        to start
+      </p>
     </ScreenOverlay>
     <ScreenOverlay v-if="GAME.state.stage === 'after'">
       <p>
-        <span class="text-3xl">Game Over</span> <br />Press
-        <span class="bg-orange-500 px-2">ENTER</span> to start
+        <span class="text-3xl">Game Over</span> <br />Press<br />
+        <span class="bg-orange-500 px-2">ENTER</span><br />
+        to restart
       </p>
     </ScreenOverlay>
-    <canvas id="boardCanvas" ref="canvas" :width="CANVAS_WIDTH" :height="CANVAS_HEIGHT"></canvas>
+    <canvas
+      id="boardCanvas"
+      ref="canvas"
+      :width="CANVAS_WIDTH"
+      :height="CANVAS_HEIGHT"
+    ></canvas>
   </div>
 </template>
