722f05411d1b8a36652ed1a348772d5537ce0b28
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2023-04-17 07:52:10 +0200

    ...

--- src/controls.js
+++ src/controls.js
@@ -1,32 +1,32 @@
 import { useGameStore } from '@/stores/game.js'
 
 export function handleKeyDown({ code }) {
-  const game = useGameStore()
-  const tetromino = game.tetromino.current
+  const GAME = useGameStore()
+  const TETROMINO = GAME.tetromino.current
 
   switch (code) {
     case 'ArrowLeft':
     case 'KeyA':
-      tetromino.move('LEFT')
+      TETROMINO.move('LEFT')
       break
 
     case 'ArrowRight':
     case 'KeyD':
-      tetromino.move('RIGHT')
+      TETROMINO.move('RIGHT')
       break
 
     case 'ArrowDown':
     case 'KeyS':
-      tetromino.move('DOWN')
+      TETROMINO.move('DOWN')
       break
 
     case 'Space':
-      tetromino.hardDrop()
+      TETROMINO.hardDrop()
       break
 
     case 'ArrowUp':
     case 'KeyW':
-      tetromino.rotate()
+      TETROMINO.rotate()
       break
 
     default:
