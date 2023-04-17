cabf8126f95c4a2e2bb1b2d9420bad3fdb21af4b
Hannes <flaschentraeger@realitymaps.de>
2023-04-17 11:08:25 +0200

    updated tailwind classes

--- src/components/StartScreen.vue
+++ src/components/StartScreen.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="start-screen absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center text-2xl"
-    :style="{ zIndex: 10 }"
+    class="start-screen absolute top-0 left-0 w-full h-full grid place-items-center text-white text-center text-2xl"
+    :style="{ zIndex: 1 }"
   >
     Press SPACE to start
   </div>
