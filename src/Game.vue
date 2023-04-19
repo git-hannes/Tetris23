8995fe684f50e843fb915df6dbb11b94abfcad43
Hannes <flaschentraeger@realitymaps.de>
2023-04-19 12:07:05 +0200

    not needed here

--- src/Game.vue
+++ src/Game.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { onMounted, ref, onUnmounted } from 'vue'
-import { handleKeyDown } from '@/controls.js'
-import { CANVAS_WIDTH, CANVAS_HEIGHT } from '@/constants/misc.js'
-import { drawBoard, drawTetromino, drawGhost } from '@/game/draw.js'
+import { onMounted, ref, onUnmounted } from "vue";
+import { handleKeyDown } from "@/controls.js";
+import { CANVAS_WIDTH, CANVAS_HEIGHT } from "@/constants/misc.js";
+import { drawBoard, drawTetromino, drawGhost } from "@/game/draw.js";
 
-import { useGameStore, useSettingsStore } from '@/stores'
+import { useGameStore, useSettingsStore } from "@/stores";
 
 import {
   GameOverScreen,
@@ -13,75 +13,64 @@ import {
   SettingsScreen,
   Sidebar,
   StartScreen,
-  Timer
-} from '@/components'
+} from "@/components";
 
-const GAME = useGameStore()
-const SETTINGS = useSettingsStore()
-let canvas = ref(null)
-let lastAnimationFrameTime = 0
+const GAME = useGameStore();
+const SETTINGS = useSettingsStore();
+let canvas = ref(null);
+let lastAnimationFrameTime = 0;
 
-const showSettings = ref(false)
-const showCollapsible = ref(false)
+const showSettings = ref(false);
+const showCollapsible = ref(false);
 
 const toggleCollapsible = () => {
-  showCollapsible.value = !showCollapsible.value
-}
+  showCollapsible.value = !showCollapsible.value;
+};
 
 const toggleSettings = () => {
-  showSettings.value = !showSettings.value
-}
+  showSettings.value = !showSettings.value;
+};
 
 function gameLoop(ctx, timestamp) {
-  const deltaTime = timestamp - lastAnimationFrameTime
-  lastAnimationFrameTime = timestamp
+  const deltaTime = timestamp - lastAnimationFrameTime;
+  lastAnimationFrameTime = timestamp;
 
-  const TETROMINO = GAME.tetromino
+  const TETROMINO = GAME.tetromino;
 
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  drawBoard(ctx, GAME)
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  drawBoard(ctx, GAME);
 
-  if (GAME.state.stage === 'playing' && !GAME.state.paused) {
-    GAME.state.timer += deltaTime
+  if (GAME.state.stage === "playing" && !GAME.state.paused) {
+    GAME.state.timer += deltaTime;
 
-    drawTetromino(ctx, TETROMINO.current, TETROMINO.current.shape)
-    drawGhost(ctx, GAME, TETROMINO.current, SETTINGS)
+    drawTetromino(ctx, TETROMINO.current, TETROMINO.current.shape);
+    drawGhost(ctx, GAME, TETROMINO.current, SETTINGS);
 
-    const fallingSpeed = 1000 - GAME.stats.level * 50
+    const fallingSpeed = 1000 - GAME.stats.level * 50;
     if (timestamp - TETROMINO.lastDropTime > fallingSpeed) {
-      TETROMINO.current.move('DOWN')
-      TETROMINO.lastDropTime = timestamp
+      TETROMINO.current.move("DOWN");
+      TETROMINO.lastDropTime = timestamp;
     }
   }
 
-  requestAnimationFrame(gameLoop.bind(null, ctx))
+  requestAnimationFrame(gameLoop.bind(null, ctx));
 }
 
 onMounted(() => {
-  const ctx = canvas.value.getContext('2d')
-  gameLoop(ctx, 0)
-  window.addEventListener('keydown', (event) =>
-    handleKeyDown(event, showSettings)
-  )
-})
+  const ctx = canvas.value.getContext("2d");
+  gameLoop(ctx, 0);
+  window.addEventListener("keydown", (event) => handleKeyDown(event, showSettings));
+});
 
 onUnmounted(() => {
-  window.removeEventListener('keydown', (event) =>
-    handleKeyDown(event, showSettings)
-  )
-})
+  window.removeEventListener("keydown", (event) => handleKeyDown(event, showSettings));
+});
 </script>
 
 <template>
   <div class="grid grid-cols-[3fr,1fr] gap-2">
-    <div
-      class="game-container relative"
-      :style="{ width: `${CANVAS_WIDTH}px`, height: `${CANVAS_HEIGHT}px` }"
-    >
-      <StartScreen
-        v-if="GAME.state.stage === 'before'"
-        @openSettings="toggleSettings"
-      />
+    <div class="game-container relative" :style="{ width: `${CANVAS_WIDTH}px`, height: `${CANVAS_HEIGHT}px` }">
+      <StartScreen v-if="GAME.state.stage === 'before'" @openSettings="toggleSettings" />
       <PauseScreen v-if="GAME.state.paused" @openSettings="toggleSettings" />
 
       <GameOverScreen v-if="GAME.state.stage === 'gameOver'" />
@@ -89,21 +78,12 @@ onUnmounted(() => {
       <ScreenOverlay v-if="showSettings">
         <SettingsScreen @close="toggleSettings" />
       </ScreenOverlay>
-      <canvas
-        id="boardCanvas"
-        ref="canvas"
-        :width="CANVAS_WIDTH"
-        :height="CANVAS_HEIGHT"
-      ></canvas>
+      <canvas id="boardCanvas" ref="canvas" :width="CANVAS_WIDTH" :height="CANVAS_HEIGHT"></canvas>
     </div>
     <Sidebar v-show="GAME.state.stage !== 'before'">
       <button @click="toggleCollapsible">
-        <span v-show="!showCollapsible" class="material-icons text-xl"
-          >more_horiz</span
-        >
-        <span v-show="showCollapsible" class="material-icons text-xl"
-          >expand_less</span
-        >
+        <span v-show="!showCollapsible" class="material-icons text-xl">more_horiz</span>
+        <span v-show="showCollapsible" class="material-icons text-xl">expand_less</span>
       </button>
 
       <div v-show="showCollapsible"></div>
